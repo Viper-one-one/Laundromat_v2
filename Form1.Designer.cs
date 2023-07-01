bc6781f978c3fd34042344382435891bb48dcc61
Viper-one-one <taylor.j.nastally@gmail.com>
2023-06-30 17:05:52 -0700

    making server connections 1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.Enter = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.Enter);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 266;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // Enter
+            // 
+            this.Enter.AutoSize = true;
+            this.Enter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.Enter.Location = new System.Drawing.Point(0, 427);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(266, 23);
+            this.Enter.TabIndex = 0;
+            this.Enter.Text = "button1";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 389);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button Enter;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace Laundromat_v2
 {
     internal static class Program
     {
+        private static MySqlConnection connection;
+        private static string server;
+        private static string database;
+        private static string user;
+        private static string password;
+        private static string port;
+        private static  string connectionString;
+        private static string sslM;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -17,6 +30,22 @@ namespace Laundromat_v2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
+        }
+
+        static int DBDelete()
+        {
+            return 0;
+        }
+
+        static int DBUpdate()
+        {
+            return 0;
+        }
+
+        static int DBInsert()
+        {
+            
         }
     }
 }
